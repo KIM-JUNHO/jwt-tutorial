99f7d252dde66b94e07836a3b50707ce45191239
prpr48 <prpr48@naver.com>
2020-03-25 23:55:54 +0900

    add /login with jsonwebtoken

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,12 @@ const cors = require('cors');
 const { verify } = require('jsonwebtoken');
 const { hash, compare } = require('bcryptjs');
 const { fakeDB } = require('./fakeDB');
+const {
+  createAccessToken,
+  createRefreshToken,
+  sendAccessToken,
+  sendRefreshToken
+} = require('./token');
 
 // 1. Register a user
 // 2. Login a user
@@ -27,10 +33,6 @@ server.use(
 // Needed to be able to read body data
 server.use(express.json()); // to support JSON-encoded bodies
 server.use(express.urlencoded({ extended: true })); // support URL-encoded bodies
-
-server.listen(process.env.PORT, () =>
-  console.log(`Server listening on port ${process.env.PORT}`)
-);
 
 // 1. Register a user
 server.post('/register', async (req, res) => {
@@ -56,3 +58,31 @@ server.post('/register', async (req, res) => {
     res.send({ error: `${err.message}` });
   }
 });
+
+// 2. Login a user
+server.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    // 1. Find user in "database". If not exist send error
+    const user = fakeDB.find(user => user.email === email);
+    if (!user) throw new Error('User does not exist');
+    // 2. Compare crypted password and see if it checks out. Send error if not
+    const valid = await compare(password, user.password);
+    if (!valid) throw new Error('Password not correct');
+    // 3. Create Refresh- and Accesstoken
+    const accesstoken = createAccessToken(user.id);
+    const refreshtoken = createRefreshToken(user.id);
+    // 4. Puth the refreshtoken in the "database"
+    user.refreshtoken = refreshtoken;
+    console.log(fakeDB);
+    // 5. Send token. Refreshtoken as a cookie and accesstoken as a regular response
+    sendRefreshToken(res, refreshtoken);
+    sendAccessToken(res, req, accesstoken);
+  } catch (err) {
+    res.send({ error: `${err.message}` });
+  }
+});
+
+server.listen(process.env.PORT, () =>
+  console.log(`Server listening on port ${process.env.PORT}`)
+);
