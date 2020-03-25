184c6b32265306e774a1c401f5f09aaa8c9a4be5
prpr48 <prpr48@naver.com>
2020-03-25 22:11:58 +0900

    add post /users/login

--- index.ts
+++ index.ts
@@ -12,15 +12,30 @@ app.get('/users', (req, res) => {
 
 app.post('/users', async (req, res) => {
   try {
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    console.log(salt);
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
     console.log(hashedPassword);
     const user = { name: req.body.name, password: hashedPassword };
     users.push(user);
     res.status(201).send();
   } catch (err) {
     console.log(err);
+  }
+});
+
+app.post('/users/login', async (req, res) => {
+  const user = users.find(user => (user.name = req.body.name));
+  if (!user) {
+    return res.status(400).send('Cannot find user');
+  }
+
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send('Success');
+    } else {
+      res.send('Not Allowed');
+    }
+  } catch {
+    res.status(500).send();
   }
 });
 
