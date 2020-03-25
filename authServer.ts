b238a2cd59911b9f47e2514df94071feed4c958d
prpr48 <prpr48@naver.com>
2020-03-25 22:57:47 +0900

    add /token to refreshtoken

--- authServer.ts
+++ authServer.ts
@@ -6,6 +6,23 @@ const jwt = require('jsonwebtoken');
 
 app.use(express.json());
 
+app.post('/token', (req, res) => {
+  const refreshToken = req.body.token;
+  if (!refreshToken) {
+    return res.sendStatus(401);
+  }
+  if (!refreshTokens.includes(refreshToken)) {
+    return res.sendStatus(403);
+  }
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+    const accessToken = generateAccessToken({ name: user.name });
+    res.json({ accessToken: accessToken });
+  });
+});
+
 app.post('/login', (req, res) => {
   // Authenticate User
 
