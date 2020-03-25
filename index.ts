3890fa8ee8ce488dadde22957f5481ca7077a694
prpr48 <prpr48@naver.com>
2020-03-25 21:47:53 +0900

    add post /users

--- index.ts
+++ index.ts
@@ -9,4 +9,10 @@ app.get('/users', (req, res) => {
   res.json(users);
 });
 
+app.post('/users', (req, res) => {
+  const user = { name: req.body.name, password: req.body.password };
+  users.push(user);
+  res.status(201).send();
+});
+
 app.listen(3000);
