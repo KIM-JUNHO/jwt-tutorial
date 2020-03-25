2a293b158faec4a77264a6c1dc71b7cdb96f00f5
prpr48 <prpr48@naver.com>
2020-03-25 23:12:17 +0900

    how to work

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,9 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const { verify } = require('jsonwebtoken');
 const { hash, compare } = require('bcryptjs');
+
+// 1. Register a user
+// 2. Login a user
+// 3. Logout a user
+// 4. Setup a protected route
+// 5. Get a new accesstoken with a refresh token
