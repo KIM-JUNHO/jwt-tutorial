7205f3997ef7c7d156a7006fa8c1646262205ea0
prpr48 <prpr48@naver.com>
2020-03-25 23:20:35 +0900

    server start

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,23 @@ const { hash, compare } = require('bcryptjs');
 // 3. Logout a user
 // 4. Setup a protected route
 // 5. Get a new accesstoken with a refresh token
+
+const server = express();
+
+// Use express middleware for easier cookie handling
+server.use(cookieParser());
+
+server.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  })
+);
+
+// Needed to be able to read body data
+server.use(express.json()); // to support JSON-encoded bodies
+server.use(express.urlencoded({ extended: true })); // support URL-encoded bodies
+
+server.listen(process.env.PORT, () =>
+  console.log(`Server listening on port ${process.env.PORT}`)
+);
